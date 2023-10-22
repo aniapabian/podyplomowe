e8059b1ea73de58ab95e4858d94bc06e505221b6
aniapabian <annapabian13@gmail.com>
2023-10-22 12:45:45 +0200

    nowa linijka

--- podyplomowe.py
+++ podyplomowe.py
@@ -1,3 +1,6 @@
 name = "programista python"
 uczelnia= "WSZIB"
 print("Aktualnie uczę się na kierunku", name, "na uczelni", uczelnia)
+zajecia="zarządzanie projektem."
+
+print("Te zajęcia nazywają się", zajecia)
